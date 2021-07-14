e9d36aa37d6736e4868fde462dddc60bdcba0d7c
anonim <mertcaliskanlinux@gmail.com>
2021-07-14 22:27:44 +0300

    DjangoSchoolWebsite

---
+++ teachers/admin.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from .models import Teacher
+
+# Register your models here.
+
+@admin.register(Teacher)
+
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
